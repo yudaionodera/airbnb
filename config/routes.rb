f6c57b354fb2a76d87625862948c0dcb7913e07d
yudaionodera <yuudai.2451@gmail.com>
2018-02-03 16:08:33 +0900

    ルーティングの名前変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-     resources :mains, only: [:index, :create]
-     root to: 'mains#index'
+     resources :room, only: [:index, :create, :new, :edit, :show, :update, :destroy]
+     root to: 'rooms#index'
 end
