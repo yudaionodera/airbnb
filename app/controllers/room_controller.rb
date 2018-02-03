7c48b7ff830668abf8c5ff12da1f773e477a778b
yudaionodera <yuudai.2451@gmail.com>
2018-02-03 16:08:49 +0900

    新しいルーティングのアクション設定

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,22 @@
+class RoomController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update  #ここもいらないかも。
+  end
+
+  def destroy  #ここいらないかも。画面遷移しないから。
+  end
+
+
+  end
+end
