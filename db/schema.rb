29412cc5bc942ab5af0eb7095b4c3a40596858ed
yudaionodera <yuudai.2451@gmail.com>
2018-03-07 21:10:52 +0900

    データベース設計

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305095439) do
+ActiveRecord::Schema.define(version: 20180307115401) do
+
+  create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.text "image", null: false
+    t.text "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
